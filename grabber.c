a5627564daa1171f9fa1c318fdaec49186b38e49
parasdhand4 <56449074+parasdhand4@users.noreply.github.com>
2020-11-30 14:53:41 -0500

    Update grabber.c

--- grabber.c
+++ grabber.c
@@ -1,15 +1,4 @@
-* V4L2 video picture grabber
-   Copyright (C) 2009 Mauro Carvalho Chehab <mchehab@infradead.org>
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation version 2 of the License.
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   Modified by Derek Molloy (www.derekmolloy.ie)
-   Modified to change resolution details and set paths for the Beaglebone.
- */
+
 
 #include <stdio.h>
 #include <stdlib.h>
